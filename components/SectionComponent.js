0a32a5b78dc3c5fafe36273c8ad0a1520ab16c90
Zubnormal Numberz <33485885+zubnormalnumberz@users.noreply.github.com>
2025-08-02 17:43:27 +0200

    SectionComponent column size

--- components/SectionComponent.js
+++ components/SectionComponent.js
@@ -1,19 +1,9 @@
 export default {
     props: {
-        title: {
-            type: String,
-            required: false
-        },
-        border: {
-            type: Boolean,
-            required: false,
-            default: true
-        },
-        collapsible: {
-            type: Boolean,
-            required: false,
-            default: false
-        }
+        title: { type: String },
+        border: { type: Boolean, default: true },
+        collapsible: { type: Boolean, default: false },
+        width: { type: Number, default: 12 },
     },
     data() {
         return {
@@ -21,25 +11,36 @@ export default {
         };
     },
     template: `
-        <div class="card m-2" :class="{ 'no-border-card': !border }">
-            <div class="card-header" v-if="title != null && border && collapsible" data-bs-toggle="collapse" role="button" aria-expanded="true" :aria-controls="collapseId" :data-bs-target="'#' + collapseId">
-                {{title}}
-            </div>
-            <div class="card-header" v-if="title != null && border && !collapsible">
-                {{title}}
-            </div>
-            <div v-if="collapsible" class="collapse show" :id="collapseId">
-                <div class="card-body container-fluid row row-cols-12"">
-                    <div class="row row-cols-12">
-                        <slot>Contenido por defecto</slot>
+        <div v-bind:class="classObject">
+            <div class="card" :class="{ 'no-border-card': !border }">
+                <div class="card-header" v-if="title != null && border && collapsible" data-bs-toggle="collapse" role="button" aria-expanded="true" :aria-controls="collapseId" :data-bs-target="'#' + collapseId">
+                    {{title}}
+                </div>
+                <div class="card-header" v-if="title != null && border && !collapsible">
+                    {{title}}
+                </div>
+                <div v-if="collapsible" class="collapse show" :id="collapseId">
+                    <div class="card-body">
+                        <div class="container-card w-auto">
+                            <div class="row row-cols-12">
+                                <slot>Contenido por defecto</slot>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div v-else class="card-body">
+                    <div class="container-card w-auto">
+                        <div class="row row-cols-12">
+                            <slot>Contenido por defecto</slot>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div v-else class="card-body">
-                <div class="container-fluid row row-cols-12 w-auto">
-                    <slot>Contenido por defecto</slot>
-                </div>
-            </div>
         </div>
-    `
+    `,
+    computed: {
+        classObject: function () {
+            return ['col-' + this.width]
+        },
+    }
 };
