aca55e13365ec34d3a13e2bd158ad8cc4909df92
Zubnormal Numberz <33485885+zubnormalnumberz@users.noreply.github.com>
2025-08-02 18:38:30 +0200

    Collapsible with Bootstrap methods

--- components/SectionComponent.js
+++ components/SectionComponent.js
@@ -5,33 +5,18 @@ export default {
         collapsible: { type: Boolean, default: false },
         width: { type: Number, default: 12 },
     },
-    data() {
-        return {
-            collapseId: 'collapse' + Math.random().toString(36).substring(2, 11),
-        };
-    },
     template: `
-        <div v-bind:class="classObject">
+        <div :class="classObject">
             <div class="card" :class="{ 'no-border-card': !border }">
-                <div class="card-header" v-if="title != null && border && collapsible" data-bs-toggle="collapse" role="button" aria-expanded="true" :aria-controls="collapseId" :data-bs-target="'#' + collapseId">
-                    {{title}}
+                <div v-if="title != null && border" class="card-header" :role="collapsible ? 'button' : null" @click="collapsible ? toggle() : null">
+                    {{ title }}
                 </div>
-                <div class="card-header" v-if="title != null && border && !collapsible">
-                    {{title}}
-                </div>
-                <div v-if="collapsible" class="collapse show" :id="collapseId">
+                <div :id="collapseId" class="collapse show">
                     <div class="card-body">
                         <div class="container-card w-auto">
                             <div class="row row-cols-12">
                                 <slot>Contenido por defecto</slot>
                             </div>
-                        </div>
-                    </div>
-                </div>
-                <div v-else class="card-body">
-                    <div class="container-card w-auto">
-                        <div class="row row-cols-12">
-                            <slot>Contenido por defecto</slot>
                         </div>
                     </div>
                 </div>
@@ -42,5 +27,26 @@ export default {
         classObject: function () {
             return ['col-' + this.width]
         },
-    }
+    },
+    mounted() {
+        const el = document.getElementById(this.collapseId);
+        if (this.collapsible) {
+            this.collapseInstance = new bootstrap.Collapse(el, {
+                toggle: false
+            });
+        }
+    },
+    methods: {
+        toggle() {
+            if (this.collapsible && this.collapseInstance) {
+                this.collapseInstance.toggle();
+            }
+        },
+    },
+    data() {
+        return {
+            collapseId: 'collapse-' + Math.random().toString(36).substring(2, 11),
+            collapseInstance: null,
+        };
+    },
 };
