5d2a1b4c0b9c4744c878a75391a2b428c441e29a
Zubnormal Numberz <33485885+zubnormalnumberz@users.noreply.github.com>
2025-08-06 20:59:43 +0200

    Field Padding FIX

--- components/FieldComponent.js
+++ components/FieldComponent.js
@@ -1,27 +1,16 @@
 export default {
     props: {
-        label: {
-            type: String,
-            required: false
-        },
-        labelWidth: {
-            type: Number,
-            required: false,
-            default: 1
-        },
-        inputWidth: {
-            type: Number,
-            required: false,
-            default: 1
-        }
+        label: { type: String, },
+        labelWidth: { type: Number, default: 1 },
+        inputWidth: { type: Number, default: 1 },
+        StartNewLine: { type: Boolean },
+        HideExpression: { type: Boolean }, // Hau berez Expression da, moldatuko da gero. Importantiena hemen estilue
     },
     template: `
-            <div style="display:contents">
-                <label class="col-form-label col-form-label-sm pe-2 text-end" v-bind:class="labelClassObject">{{label}}</label>
-                <div v-bind:class="inputClassObject" class="pe-1 py-1">
-                    <slot>Input</slot>
-                </div>
-            </div>
+        <label class="col-form-label col-form-label-sm pe-2 text-end align-self-start" v-bind:class="labelClassObject">{{label}}</label>
+        <div v-bind:class="inputClassObject">
+            <slot>Input</slot>
+        </div>
     `,
     computed: {
         labelClassObject() {
